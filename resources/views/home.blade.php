407a2004d81888ae97c929aec93be939de51a983
Matteo Polsinelli <netrider91@gmail.com>
2017-12-16 17:38:26 +0100

    fix home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Monthy's webservice </title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
-<div class="container">
+@extends('welcome')
+
+@section('title', 'Welcome to Monthy's webservice')
+
+@section('content')
+
     <h2>Monthy's webservice</h2><br/>
         <div class="row">
             <div class="col-md-4">
@@ -14,8 +11,4 @@
             <button class="btn btn-default" onclick="location.href='/listPiatti';">Gestione Piatti</button>
             <button class="btn btn-default" onclick="location.href='/listEventi';">Gestione Eventi</button>
         </div>
-        </div>
-</div>
-</div>
-</body>
-</html>
+@endsection
